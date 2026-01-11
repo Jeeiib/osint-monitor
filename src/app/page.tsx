64043fed4edd93af770d32fd5151c941cb35c6bf
Jean-Baptiste <renartjeanbaptiste@gmail.com>
2026-01-11 12:41:06 +0100

    feat: add filter bar with category toggles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { BaseMap } from "@/components/map";
 import { MainLayout } from "@/components/layout";
+import { FilterBar } from "@/components/filters";
 
 export default function Home() {
   return (
     <MainLayout>
       <BaseMap className="absolute inset-0" />
+      <FilterBar />
     </MainLayout>
   );
 }
